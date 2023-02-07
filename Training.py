61e93a6e5591daa89b9ba2b4a959a0061eb35ce3
MarMannnn <m.maniero.97@gmail.com>
2023-02-07 19:14:26 +0100

    updated

--- Training.py
+++ Training.py
@@ -2,7 +2,6 @@ import DataLoader
 import Model
 
 # prepare the dataset
-<<<<<<< HEAD
 x_train, y_train, x_test, y_test, x_valid, y_valid = DataLoader.DataLoader()
 
 # get the model
@@ -16,9 +15,3 @@ model.compile(optimizer=opt,
               loss=loss, #true because values [-1,1]
               metrics=[keras.metrics.BinaryAccuracy()])
 model.fit(x_train, y_train, epochs=20, validation_data=(x_valid, y_valid), verbose=1)
-=======
-
-# get the model
-
-# get the hyperparms for the training
->>>>>>> 476fbe1058e99b2025e44a5c8cdcaff5361ba12a
